677b99922ecc321c047204189b24f2cbf5516db9
nivlekp <osamupang@gmail.com>
2024-03-06 16:44:37 +1100

    teach library to make a copy of the sections

--- unicorns/library.py
+++ unicorns/library.py
@@ -1,3 +1,6 @@
+import pathlib
+import shutil
+
 import abjad
 
 
@@ -11,3 +14,12 @@ def make_empty_score():
     piano_music_staff = abjad.Staff([piano_music_voice], name="Piano.Staff")
     score = abjad.Score([piano_music_staff], name="Score")
     return score
+
+
+def move_music_ily_from_segment_directory_to_build_directory(segment_name):
+    segment_directory = pathlib.Path() / "unicorns" / "segments" / segment_name
+    music_ily_path = segment_directory / "music.ily"
+    _sections_path = segment_directory.parents[1] / "builds" / "score" / "_sections"
+    target_name = segment_directory.stem + ".ily"
+    target_path = _sections_path / target_name
+    shutil.copy(music_ily_path, target_path)
